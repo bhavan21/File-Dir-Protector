3c5caf7170ae4dab6ea0dc2526a7a9b66e58bc50
Bhavan Turaka <bhavanturaka21@gmail.com>
2018-03-18 00:06:48 +0530

    Initial commit

---
+++ crypto.py
@@ -0,0 +1,104 @@
+import sys,os
+import hashlib, uuid
+import getpass
+
+
+
+def string2bits(s=''):
+    return [bin(ord(x))[2:].zfill(8) for x in s]
+
+def getInt(p=''):
+	output=0
+	for x in p:
+		output+=ord(x)
+	return output
+
+def bits2string(b=None):
+    return ''.join([chr(int(x, 2)) for x in b])
+
+def rotate(l, n):
+    return l[n:] + l[:n]
+
+
+def encrypt(file,n=0):
+	f = open(file)
+	data = ""
+	for line in f:
+		data = data + line
+	lenData=len(data)
+	if data[lenData-1]=='0' or data[lenData-1]=='1':
+		return False
+	list = string2bits(data)
+	list.append("00000001")
+	l = len(list)
+	n=n%l
+	list = rotate(list,n)
+	encData = ''.join(list)
+	temp = sys.stdout
+	sys.stdout = open(file,'w')
+	sys.stdout.write(encData)
+	sys.stdout = temp
+	return True
+
+def decrypt(file,n=0):
+	f = open(file)
+	data = ""
+	for line in f:
+		data = data + line
+	lenData=len(data)
+	if data[lenData-1]!='0' and data[lenData-1]!='1':
+		return False
+	list = [data[i:i+8] for i in range(0, len(data), 8)]
+	l = len(list)
+	n=n%l
+	list = rotate(list,l-n)
+	list.pop()
+	decData = bits2string(list)
+	temp = sys.stdout
+	sys.stdout = open(file,'w')
+	sys.stdout.write(decData)
+	sys.stdout = temp
+	return True
+
+def validate(password):
+	salt = "395fb70b393b42c58f98d172b97fc759" #uuid.uuid4().hex
+	hashed_password = hashlib.sha512(password + salt).hexdigest()
+	if hashed_password=="8affa51aadaf8cc5d0dd78d9b8eae59221e5c0db55f3e7c7800f41c366be86afeb6f14fa0faede86a65b5614e13820c53164aade796c5892c17c781115332276":
+		return True
+	else:
+		return False
+
+def process(file,n):
+	if sys.argv[2]=="e":
+		if encrypt(file,n):
+			print(file+" encrypted succesfully")
+		else:
+			print(file+" seems to be already encrypted")
+	if sys.argv[2]=="d":
+		if decrypt(file,n):
+			print(file+" decrypted succesfully")
+		else:
+			print(file+" seems to be already decrypted")
+
+password = getpass.getpass()
+if validate(password):
+	if len(sys.argv)<3:
+		print("Expected 2 arguments")
+		exit()
+	n = getInt(password)
+	if os.path.isfile(sys.argv[1]):
+		process(sys.argv[1],n)
+		exit()
+	if os.path.isdir(sys.argv[1]):
+		path = sys.argv[1]
+		for file in os.listdir(path):
+			file = os.path.join(path, file)
+			process(file,n)
+		exit()
+	print("Wrong file/dir path")
+else:
+	print("Wrong Password!")
+
+
+
+
